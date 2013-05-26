00786481be3b0b81893becc6ceebe93a8606b888
Luna Malacova <luna.malacova@gmail.com>
2013-05-25 21:50:17 -0700

    added default / page

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	'/home/elfette/py/django/pomodoro/pomodoro/pomodoro'
 )
 
 INSTALLED_APPS = (
@@ -120,10 +121,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'django.contrib.webdesign',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'app',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -153,4 +156,5 @@ LOGGING = {
             'propagate': True,
         },
     }
+
 }
