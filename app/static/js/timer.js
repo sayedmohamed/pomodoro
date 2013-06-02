a090b3f085c0cd8a751e246f22ab4a0c98a11e38
Luna Malacova <luna.malacova@gmail.com>
2013-06-02 13:38:41 -0700

    Added base javascript

---
+++ app/static/js/timer.js
@@ -0,0 +1,24 @@
+/*
+var start = new Date;
+
+setInterval(function() {
+    $('.Timer').text((new Date - start) / 1000 + " Seconds");
+}, 1000);
+*/
+
+// Length of our timers
+// 25 minutes, 5 minutes, 10 minutes
+pomodoroInterval = 1500000;
+shortBreakInterval = 300000;
+longBreakInterval = 900000;
+
+moveOn = false; // If we want to move on 
+
+// Pieces of text
+
+completedText = "You are finished!";
+
+function startTimer(interval) {
+
+}
+	
