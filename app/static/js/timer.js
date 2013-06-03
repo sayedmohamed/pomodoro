a21e99dad67c3d493f3c01bcb746de13272f2a91
Luna Malacova <luna.malacova@gmail.com>
2013-06-02 23:22:04 -0700

    made short break 5 minutes again

--- app/static/js/timer.js
+++ app/static/js/timer.js
@@ -1,7 +1,7 @@
 // Length of our timers
 // 25 minutes, 5 minutes, 10 minutes
 pomodoroInterval = 1500000;
-shortBreakInterval = 3000;
+shortBreakInterval = 300000;
 longBreakInterval = 900000;
 
 moveOn = false; // If we want to move on to breaks automatically
